1629d7758a96c06f9a88acbfafb33524686655dc
TrisKnightGamer <trihd123@gmail.com>
2020-12-01 21:36:02 +0700

    Updated

--- frontend/src/components/SendMessageForm/index.js
+++ frontend/src/components/SendMessageForm/index.js
@@ -50,7 +50,7 @@ export default class SendMessageForm extends Component  {
         if (this.state.message === "") {
             return
         } else {
-            fetch('http://0.0.0.0:5000/api/v1/messages', {
+            fetch('https://0.0.0.0:5000/api/v1/messages', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -69,7 +69,7 @@ export default class SendMessageForm extends Component  {
                         .then(postResponse => {
                             console.log(postResponse);
                             
-                            fetch('http://0.0.0.0:5000/api/v1/messages/reply', {
+                            fetch('https://0.0.0.0:5000/api/v1/messages/reply', {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json',
@@ -124,7 +124,7 @@ export default class SendMessageForm extends Component  {
         if (this.state.message === "") {
             return
         } else {
-            fetch('http://0.0.0.0:5000/api/v1/messages', {
+            fetch('https://0.0.0.0:5000/api/v1/messages', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -143,7 +143,7 @@ export default class SendMessageForm extends Component  {
                         .then(postResponse => {
                             console.log(postResponse);
                             
-                            fetch('http://0.0.0.0:5000/api/v1/messages/reply', {
+                            fetch('https://0.0.0.0:5000/api/v1/messages/reply', {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json',
